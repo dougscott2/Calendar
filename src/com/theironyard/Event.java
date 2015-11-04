d49da327d816b1118d15581621d6ec5e6a08794f
DrScott <dougscott2@gmail.com>
2015-11-04 11:52:03 -0500

    created insertEvents and selectEvents with passing tests

---
+++ src/com/theironyard/Event.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by DrScott on 11/4/15.
+ */
+public class Event {
+    String description;
+    LocalDateTime startDate;
+    int id;
+}
